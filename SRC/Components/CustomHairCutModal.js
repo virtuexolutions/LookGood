e9c1bdda2049b4938ec6b35ad4df7d94ed479a58
naneeta1 <naneeta.bcss19@iba-suk.edu.pk>
2023-07-31 15:10:25 +0500

    ImagesScreen updated

--- SRC/Components/CustomHairCutModal.js
+++ SRC/Components/CustomHairCutModal.js
@@ -13,6 +13,8 @@ import {windowHeight, windowWidth} from '../Utillity/utils';
 import CustomText from './CustomText';
 import CustomImage from './CustomImage';
 import Slider from '@react-native-community/slider';
+import Draggable from 'react-native-draggable';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const CustomHairCutModal = ({
   data,
@@ -42,59 +44,107 @@ const CustomHairCutModal = ({
         setStyle({});
         setShow(false);
       }}>
-      <View
-        style={{
-          backgroundColor: Color.white,
-          height: Dimensions.get('window').height * 0.9,
-          width: Dimensions.get('window').width * 0.9,
-          //   paddingHorizontal: moderateScale(10, 0.3),
-          //   paddingVertical: moderateScale(10, 0.3),
-          borderRadius: Dimensions.get('window').width * 0.02,
-        }}>
-        <ImageBackground
+      <GestureHandlerRootView>
+        <View
           style={{
-            width: windowWidth * 0.9,
-            height: windowHeight * 0.6,
-            justifyContent: 'center',
-            alignItems: 'center',
-            //   backgroundColor: 'blue',
-            //   alignSelf: 'center',1
-            //   marginTop: windowHeight * 0.2,
-          }}
-          resizeMode={'stretch'}
-          source={{uri: image?.uri}}>
-          <CustomImage
-            source={style.image}
+            backgroundColor: Color.white,
+            height: Dimensions.get('window').height * 0.9,
+            width: Dimensions.get('window').width * 0.9,
+            //   paddingHorizontal: moderateScale(10, 0.3),
+            //   paddingVertical: moderateScale(10, 0.3),
+            borderRadius: Dimensions.get('window').width * 0.02,
+          }}>
+          <ImageBackground
             style={{
-              width: size+10,
-              height: size,
-              transform: [{ rotate: `${rotation}deg`}]
+              width: windowWidth * 0.9,
+              height: windowHeight * 0.6,
+              justifyContent: 'center',
+              alignItems: 'center',
+              //   backgroundColor: 'blue',
+              //   alignSelf: 'center',1
+              //   marginTop: windowHeight * 0.2,
             }}
-          />
-        </ImageBackground>
-        <Slider
-          style={{width: 200, height: 40}}
-          minimumValue={40}
-          maximumValue={150}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-          onValueChange={(data)=>{
-            console.log(data)
-            setSize(data)
-          }}
-        />
-          <Slider
-          style={{width: 200, height: 40}}
-          minimumValue={1}
-          maximumValue={360}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-          onValueChange={(data)=>{
-            console.log(data)
-            setRotation(data)
-          }}
-        />
-      </View>
+            resizeMode={'stretch'}
+            source={{uri: image?.uri}}>
+            {/* <Draggable
+            imageSource={require('../Assets/Images/MenHair/1.png')}
+            renderSize={80}
+            x={200}
+            y={400}
+            
+          /> */}
+            {/* <View style={{backgroundColor:'white', position:'absolute', alignItems:'center', justifyContent:'center',zIndex:1}}>
+              <Draggable x={200} y={200}>
+                <CustomImage
+                  source={style.image}
+                  style={{
+                    width: size + 10,
+                    height: size,
+                    transform: [{rotate: `${rotation}deg`}],
+                  }}
+                />
+              </Draggable>
+            </View> */}
+          </ImageBackground>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <CustomText
+              style={{
+                width: windowWidth * 0.15,
+                fontSize: moderateScale(15, 0.6),
+              }}
+              isBold>
+              size
+            </CustomText>
+
+            <Slider
+              style={{width: 200, height: 40}}
+              minimumValue={40}
+              maximumValue={150}
+              value={95}
+              minimumTrackTintColor={Color.themeColor}
+              maximumTrackTintColor={Color.themeColor}
+              thumbTintColor={Color.themeColor1}
+              onValueChange={data => {
+                console.log(data);
+                setSize(data);
+              }}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <CustomText
+              style={{
+                width: windowWidth * 0.15,
+                fontSize: moderateScale(15, 0.6),
+              }}
+              isBold>
+              rotate
+            </CustomText>
+            <Slider
+              style={{width: 200, height: 40}}
+              minimumValue={90}
+              value={180}
+              maximumValue={270}
+              thumbTintColor={Color.themeColor1}
+              minimumTrackTintColor={Color.themeColor}
+              maximumTrackTintColor={Color.themeColor}
+              onValueChange={data => {
+                console.log(data);
+                setRotation(data);
+              }}
+            />
+          </View>
+        </View>
+      </GestureHandlerRootView>
     </Modal>
   );
 };
--- SRC/Screens/ImageScreen.js
+++ SRC/Screens/ImageScreen.js
@@ -1,5 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import CustomText from '../Components/CustomText';
 import CustomImage from '../Components/CustomImage';
 import {windowHeight, windowWidth} from '../Utillity/utils';
@@ -10,16 +16,34 @@ import Color from '../Assets/Utilities/Color';
 import CustomButton from '../Components/CustomButton';
 import ImagePickerModal from '../Components/ImagePickerModal';
 import CustomHairCutModal from '../Components/CustomHairCutModal';
+import {Icon} from 'native-base';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Draggable from 'react-native-draggable';
+import Slider from '@react-native-community/slider';
+import ViewShot from 'react-native-view-shot';
 
 const ImageScreen = props => {
   const data = props?.route?.params?.data;
   const Image = props?.route?.params?.image;
+  console.log('🚀 ~ file: ImageScreen.js:28 ~ ImageScreen ~ Image:', Image);
+  const ref = useRef();
+
+  const [size, setSize] = useState(130);
+  const [rotation, setRotation] = useState(0);
 
   const [showModal, setShowModal] = useState(false);
   const [image, setImage] = useState(Image);
   const [selectedImage, setSelectedImage] = useState({});
   const [isVisible, setIsVisible] = useState(false);
+  console.log(
+    '🚀 ~ file: ImageScreen.js:35 ~ ImageScreen ~ isVisible:',
+    isVisible,
+  );
   const [selectedStyle, setSelectedStyle] = useState({});
+  console.log(
+    '🚀 ~ file: ImageScreen.js:25 ~ ImageScreen ~ selectedStyle:',
+    selectedStyle,
+  );
   const [array, setArray] = useState([]);
   console.log('🚀 ~ file: ImageScreen.js:24 ~ ImageScreen ~ array:', array);
   const [selectedCat, setSelectedCat] = useState('');
@@ -78,12 +102,22 @@ const ImageScreen = props => {
     },
   ];
 
+  const captureShot = () => {
+    // on mount
+    ref.current.capture().then(uri => {
+      console.log('do something with ', uri);
+      setImage({uri});
+      setIsVisible(!isVisible);
+      setSelectedStyle({});
+      setSelectedCat('')
+    });
+  };
+
   useEffect(() => {
-    if(Object.keys(selectedStyle).length > 0){
-      setIsVisible(true)
+    if (Object.keys(selectedStyle).length > 0) {
+      setIsVisible(true);
     }
-  }, [selectedStyle])
-  
+  }, [selectedStyle]);
 
   return (
     <ScreenBoiler
@@ -109,16 +143,47 @@ const ImageScreen = props => {
             overflow: 'hidden',
             marginTop: moderateScale(30, 0.3),
           }}>
-          <CustomImage
-            source={
-              Object.keys(image).length > 0
-                ? {uri: image?.uri}
-                : require('../Assets/Images/barber.png')
-            }
-            resizeMode={'cover'}
-            style={{height: '100%', width: '100%'}}
-          />
+          <ViewShot
+            ref={ref}
+            options={{fileName: 'hello', format: 'jpg', quality: 0.9}}>
+            <ImageBackground
+              source={
+                Object.keys(image).length > 0
+                  ? {uri: image?.uri}
+                  : require('../Assets/Images/barber.png')
+              }
+              resizeMode={'cover'}
+              style={{height: '100%', width: '100%'}}>
+              <View>
+                {isVisible && (
+                  <Draggable
+                    x={100}
+                    y={100}
+                    // imageSource={selectedStyle.image}
+                    // renderSize={size + 10}
+                    // style={{
+                    //   width: size + 10,
+                    //   height: size,
+                    //   transform: [{rotate: `${rotation}deg`}],
+                    // }}
+                  >
+                    <CustomImage
+                      source={selectedStyle?.image}
+                      style={{
+                        width: size + 10,
+                        height: size,
+                        transform: [{rotate: `${rotation}deg`}],
+                      }}
+                    />
+                  </Draggable>
+                )}
+              </View>
+            </ImageBackground>
+          </ViewShot>
+
+          {/* <Draggable x={200} y={300} renderColor='red' renderText='B'/> */}
         </View>
+
         {selectedCat == '' ? (
           <>
             <CustomButton
@@ -182,35 +247,152 @@ const ImageScreen = props => {
               flexDirection: 'row',
               flexWrap: 'wrap',
               alignSelf: 'center',
+              zIndex: 1,
               // backgroundColor : 'green'
             }}>
-            {array?.map((item, index) => {
-              return (
+            <View
+              style={{
+                width: windowWidth,
+                marginTop: moderateScale(5, 0.3),
+                marginLeft: moderateScale(-10, 0.3),
+                // zIndex:2,
+              }}>
+              <Icon
+                name={'chevron-small-left'}
+                as={Entypo}
+                color={Color.themeColor}
+                size={10}
+                onPress={() => {
+                  if (isVisible) {
+                    setIsVisible(!isVisible);
+                    setSelectedStyle({});
+                  } else {
+                    setSelectedCat('');
+                  }
+                }}
+              />
+            </View>
+            {!isVisible ? (
+              array?.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      setSelectedStyle(item);
+                    }}
+                    style={{
+                      width: windowWidth * 0.3,
+                      height: windowHeight * 0.13,
+                      marginRight: windowWidth * 0.015,
+                      borderRadius: moderateScale(10, 0.6),
+                      overflow: 'hidden',
+                      backgroundColor: 'white',
+                      marginTop: moderateScale(10, 0.3),
+                      // alignItems : 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <CustomImage
+                      onPress={() => {
+                        setSelectedStyle(item);
+                      }}
+                      source={item?.image}
+                      style={{
+                        width: '90%',
+                        height: '90%',
+                      }}
+                    />
+                  </TouchableOpacity>
+                );
+              })
+            ) : (
+              <>
                 <View
                   style={{
-                    width: windowWidth * 0.3,
-                    height: windowHeight * 0.13,
-                    marginRight: windowWidth * 0.015,
-                    borderRadius: moderateScale(10, 0.6),
-                    overflow: 'hidden',
-                    backgroundColor: 'white',
-                    marginTop: moderateScale(20, 0.3),
-                    // alignItems : 'center',
-                    justifyContent : 'center',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: windowWidth,
+                    // backgroundColor:'red',
+                    // position:'absolute',
+                    zIndex: 1,
+                    marginTop: moderateScale(50, 0.3),
                   }}>
-                  <CustomImage
-                  onPress={()=>{
-                    setSelectedStyle(item)
-                  }}
-                    source={item?.image}
+                  <View
                     style={{
-                      width: '90%',
-                      height: '90%',
+                      flexDirection: 'row',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <CustomText
+                      style={{
+                        width: windowWidth * 0.15,
+                        fontSize: moderateScale(15, 0.6),
+                        color: Color.white,
+                      }}
+                      isBold>
+                      Size
+                    </CustomText>
+                    <Slider
+                      style={{width: 250, height: 40}}
+                      minimumValue={60}
+                      maximumValue={200}
+                      value={130}
+                      minimumTrackTintColor={Color.themeColor}
+                      maximumTrackColor={Color.themeColor}
+                      maximumTrackTintColor={Color.white}
+                      thumbTintColor={Color.themeColor1}
+                      onValueChange={data => {
+                        console.log(data);
+                        setSize(data);
+                      }}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <CustomText
+                      style={{
+                        width: windowWidth * 0.15,
+                        fontSize: moderateScale(15, 0.6),
+                        color: Color.white,
+                      }}
+                      isBold>
+                      rotate
+                    </CustomText>
+                    <Slider
+                      style={{width: 250, height: 40}}
+                      minimumValue={-180}
+                      value={0}
+                      maximumValue={180}
+                      thumbTintColor={Color.themeColor1}
+                      minimumTrackTintColor={Color.themeColor}
+                      maximumTrackTintColor={Color.themeColor}
+                      onValueChange={data => {
+                        console.log(data);
+                        setRotation(data);
+                      }}
+                    />
+                  </View>
+                  <CustomButton
+                    bgColor={Color.themeColor}
+                    borderColor={'white'}
+                    borderWidth={1}
+                    textColor={Color.black}
+                    width={windowWidth * 0.8}
+                    height={windowHeight * 0.06}
+                    text={'Save'}
+                    fontSize={moderateScale(14, 0.3)}
+                    isGradient={true}
+                    isBold
+                    onPress={() => {
+                      captureShot();
                     }}
+                    marginTop={moderateScale(30, 0.3)}
                   />
                 </View>
-              );
-            })}
+              </>
+            )}
           </View>
         )}
         <View
@@ -218,6 +400,8 @@ const ImageScreen = props => {
             position: 'absolute',
             bottom: 30,
             right: 0,
+            zIndex: 0,
+            // backgroundColor:'black'
           }}>
           <CustomImage
             source={require('../Assets/Images/backgroundLogo.png')}
@@ -230,7 +414,7 @@ const ImageScreen = props => {
         setShow={setShowModal}
         setFileObject={setImage}
       />
-      <CustomHairCutModal
+      {/* <CustomHairCutModal
         data={array}
         image={image}
         setImage={setImage}
@@ -238,7 +422,7 @@ const ImageScreen = props => {
         show={isVisible}
         setStyle={setSelectedStyle}
         style={selectedStyle}
-      />
+      /> */}
     </ScreenBoiler>
   );
 };
